8969e939131b85ce0d3223ed69ced2ae45ba6b1c
Ednan Pasagic <ednan.pasagic@wearespindle.com>
2018-05-03 09:40:47 +0200

    Temporary connection with local API through test API key

--- src/lib/api/setupRequestOptions.js
+++ src/lib/api/setupRequestOptions.js
@@ -5,8 +5,12 @@ if (window && window.csrf) {
 }
 
 export default function setupRequestOptions(path, _options) {
+  // TODO: Ugly hack for now to allow multiple query parameters.
+  // TODO: Remove the key parameter.
+  const key = `${path.includes('?') ? '&' : '?'}key=superuser1`;
+
   return {
-    uri: `${base}${path.replace(base, '')}`,
+    uri: `${base}${path.replace(base, '')}${key}`,
     options: Object.assign({}, defaultRequest, _options)
   };
 }
